21727cb091cfa09be7c394bdefdb73ac39b71905
criatechfc <vitorguilhermedevofc@gmail.com>
2026-07-11 18:34:27 -0300

    Update perfilRoutes.js

--- routes/perfilRoutes.js
+++ routes/perfilRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const perfilController = require('../controllers/perfilController');
 const { uploadPerfil } = require('../middlewares/upload');
+const { csrfProtection } = require('../middlewares/csrf');
 
 router.get('/', perfilController.pagina);
-router.put('/', uploadPerfil.single('foto'), perfilController.atualizarDados);
+router.put('/', uploadPerfil.single('foto'), csrfProtection, perfilController.atualizarDados);
 router.post('/senha', perfilController.alterarSenha);
 
 module.exports = router;

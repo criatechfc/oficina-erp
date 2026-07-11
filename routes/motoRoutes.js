e2b8c0f43a3204dde88d61deb788ab4885b1fe0d
criatechfc <vitorguilhermedevofc@gmail.com>
2026-07-11 18:33:34 -0300

    Update motoRoutes.js

--- routes/motoRoutes.js
+++ routes/motoRoutes.js
@@ -3,15 +3,16 @@ const router = express.Router();
 const motoController = require('../controllers/motoController');
 const { exigirPerfil } = require('../middlewares/auth');
 const { uploadMoto } = require('../middlewares/upload');
+const { csrfProtection } = require('../middlewares/csrf');
 
 router.use(exigirPerfil('administrador', 'gerente', 'atendente', 'mecanico'));
 
 router.get('/', motoController.listar);
 router.get('/novo', motoController.formularioNovo);
-router.post('/', uploadMoto.array('fotos', 6), motoController.criar);
+router.post('/', uploadMoto.array('fotos', 6), csrfProtection, motoController.criar);
 router.get('/:id', motoController.visualizar);
 router.get('/:id/editar', motoController.formularioEditar);
-router.put('/:id', uploadMoto.array('fotos', 6), motoController.atualizar);
+router.put('/:id', uploadMoto.array('fotos', 6), csrfProtection, motoController.atualizar);
 router.delete('/:id', motoController.remover);
 
 module.exports = router;

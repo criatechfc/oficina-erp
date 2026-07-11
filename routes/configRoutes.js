74e6dfc42f9c0e0f2464a67864a4fff270bb8810
criatechfc <vitorguilhermedevofc@gmail.com>
2026-07-11 18:35:08 -0300

    Update configRoutes.js

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const configController = require('../controllers/configController');
 const { exigirPerfil } = require('../middlewares/auth');
 const { uploadPerfil } = require('../middlewares/upload');
+const { csrfProtection } = require('../middlewares/csrf');
 
 router.use(exigirPerfil('administrador'));
 
 router.get('/', configController.pagina);
-router.put('/', uploadPerfil.single('logo'), configController.atualizar);
+router.put('/', uploadPerfil.single('logo'), csrfProtection, configController.atualizar);
 
 module.exports = router;

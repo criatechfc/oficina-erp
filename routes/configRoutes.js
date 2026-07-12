e2d8fde10fb00799dcd0659ac8e34a9d3bb8ae10
criatechfc <vitorguilhermedevofc@gmail.com>
2026-07-12 17:32:05 -0300

    Update configRoutes.js

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -8,6 +8,7 @@ const { csrfProtection } = require('../middlewares/csrf');
 router.use(exigirPerfil('administrador'));
 
 router.get('/', configController.pagina);
+router.get('/diagnostico-uploads', configController.diagnosticoUploads);
 router.put('/', uploadPerfil.single('logo'), csrfProtection, configController.atualizar);
 
 module.exports = router;
